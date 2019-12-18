d9ba402d9446365e1d8da2ea60fbf3ca1aa1bbf4
Enrique <enriquecardenas96@gmail.com>
2019-12-18 09:59:07 -0800

    refactor tests to be grouped in describe blocks

--- tests/blog_list_api.test.js
+++ tests/blog_list_api.test.js
@@ -15,72 +15,75 @@ beforeEach(async () => {
   await Promise.all(promiseArray)
 })
 
-test('all blogs are returned', async () => {
-  const response = await api.get('/api/blogs')
+describe('when there is initially some blogs saved', () => {
+  test('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs')
 
-  expect(response.body.length).toBe(helper.initialBlogs.length)
+    expect(response.body.length).toBe(helper.initialBlogs.length)
+  })
+
+  test('all blogs have their unique identifier property named id', async () => {
+    const response = await api.get('/api/blogs')
+    const blogs = response.body
+
+    blogs.forEach(blog => expect(blog.id).toBeDefined())
+  })
 })
 
-test('all blogs have their unique identifier property named id', async () => {
-  const response = await api.get('/api/blogs')
-  const blogs = response.body
+describe('addition of a new blog', () => {
+  test('succeeds with valid data', async () => {
+    const newBlog = {
+      title: 'jest testing title 1',
+      author: 'jest test author 1',
+      url: 'www.jestfakeaddress1.com',
+      likes: 11
+    }
 
-  blogs.forEach(blog => expect(blog.id).toBeDefined())
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
 
-})
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd.length).toBe(helper.initialBlogs.length + 1)
 
-test('a valid blog can be added', async () => {
-  const newBlog = {
-    title: 'jest testing title 1',
-    author: 'jest test author 1',
-    url: 'www.jestfakeaddress1.com',
-    likes: 11
-  }
+    const titles = blogsAtEnd.map(blog => blog.title)
+    expect(titles).toContain(
+      'jest testing title 1'
+    )
+  })
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+  test('defaults likes to 0 if missing', async () => {
+    const newBlog = {
+      title: 'jest testing title 1',
+      author: 'jest test author 1',
+      url: 'www.jestfakeaddress1.com'
+    }
 
-  const blogsAtEnd = await helper.blogsInDb()
-  expect(blogsAtEnd.length).toBe(helper.initialBlogs.length + 1)
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
 
-  const titles = blogsAtEnd.map(blog => blog.title)
-  expect(titles).toContain(
-    'jest testing title 1'
-  )
-})
+    const response = await api.get('/api/blogs')
+    const blogs = response.body
 
-test('likes property defaults to 0 if missing', async () => {
-  const newBlog = {
-    title: 'jest testing title 1',
-    author: 'jest test author 1',
-    url: 'www.jestfakeaddress1.com'
-  }
+    const lastBlogAdded = blogs[blogs.length - 1]
+    expect(lastBlogAdded.likes).toBeDefined()
+    expect(lastBlogAdded.likes).toBe(0)
+  })
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
+  test('returns 400 Bad Request if author and url are missing from post request', async () => {
+    const newBlog = {
+      title: 'jest testing title 1',
+      likes: 42
+    }
 
-  const response = await api.get('/api/blogs')
-  const blogs = response.body
-
-  const lastBlogAdded = blogs[blogs.length - 1]
-  expect(lastBlogAdded.likes).toBeDefined()
-  expect(lastBlogAdded.likes).toBe(0)
-})
-
-test('return 400 Bad Request if author and url are missing from post request', async () => {
-  const newBlog = {
-    title: 'jest testing title 1',
-    likes: 42
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(400)
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(400)
+  })
 })
 
 afterAll(() => {
