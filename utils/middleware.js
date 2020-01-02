2a9bcaa16ec15770a47f9fe4c61340f3340a1828
Enrique <enriquecardenas96@gmail.com>
2020-01-02 11:26:26 -0800

    fix token extractor middleware

--- utils/middleware.js
+++ utils/middleware.js
@@ -21,9 +21,8 @@ const tokenExtractor = (request, response, next) => {
   const authorization = request.get('authorization')
   if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
     request.token = authorization.substring(7)
-    next()
   }
-  return null
+  next()
 }
 
 module.exports = {
