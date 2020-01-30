9ee0f50b608c184018d1c00c839813d0d2212b09
Enrique <enriquecardenas96@gmail.com>
2020-01-30 13:09:21 -0800

    uibuild

---
+++ build/precache-manifest.c96f729a27e37e0c634327ccc97f9cb5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8639a60839b2d8a2dba42dc79e4468a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7316fc3c75e05a86dab",
+    "url": "/static/css/main.a0e5f6f8.chunk.css"
+  },
+  {
+    "revision": "10338763bbd355283582",
+    "url": "/static/js/2.5a857b35.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/static/js/2.5a857b35.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e7316fc3c75e05a86dab",
+    "url": "/static/js/main.53e637e4.chunk.js"
+  },
+  {
+    "revision": "c5994fda209eb59e9a0d",
+    "url": "/static/js/runtime-main.0de63f21.js"
+  }
+]);
